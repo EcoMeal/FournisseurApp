003e0b6d7414511f125b60ef246eff743383ba57
jhoukem <jhoukem@gmail.com>
2017-12-03 21:06:55 +0100

    Added category delete test and product delete test WIP

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -24,6 +24,7 @@ class FeatureContext extends WebTestCase implements Context
     {
       $this->client = static::createClient();
     }
+    
     /**
      * @Given il n'y a aucune categorie dans l'application
      */
@@ -36,6 +37,7 @@ class FeatureContext extends WebTestCase implements Context
 	$this->assertEquals(1, $crawler->filter('.card-image-label')->count(),
                 "A category already exist before the test run");
     }
+    
     /**
      * @When j'ajoute la categorie :cate dans l'application
      */
@@ -49,6 +51,7 @@ class FeatureContext extends WebTestCase implements Context
       // submit the form
       $this->client->submit($form);
     }
+    
     /**
      * @Then il y a une categorie :cate dans l'application
      */
@@ -58,19 +61,53 @@ class FeatureContext extends WebTestCase implements Context
 	
         // By default there is already one card for the category creation 
         // which has the same class
-        $category_count = $crawler->filter('.card-image-label')->reduce(
-                function ($node, $i) use($cate) {                      
-                    if (strcmp(trim($node->text()), $cate) == 0) {
-                        return true;
-                    } else {
-                        return false;
-                    }
-                }
-        )->count();
+        $category_count = $this->getItemCardCount($crawler, $cate); 
+        
 	$this->assertEquals(1, $category_count, "Category '". $cate ."' count is incorrect");
-        // Clean all the category.
-      //  $this->client->request('GET', '/category/clean');
     }
+    
+    /**
+     * @Given il existe la categorie :cate dans l'application
+     */
+    public function ilExisteLaCategorieDansLapplication($cate)
+    {
+      $crawler = $this->client->request('GET', '/category');
+      
+      $form = $crawler->selectButton('Valider')->form();
+      // Set the task values
+      $form->setValues(array('appbundle_category[name]' => $cate));
+      // submit the form
+      $this->client->submit($form);
+    }
+
+    /**
+     * @When je supprime la categorie :cate dans l'application
+     */
+    public function jeSupprimeLaCategorieDansLapplication($cate)
+    {
+        $crawler = $this->client->request('GET', '/category');
+        // Filter to find the correct onclick attribute for the product.
+        $filter = "[onclick*=\"deleteCategory('". $cate ."',\"]";       
+
+        $categoryID =  $this->getItemCardId($crawler, $filter);             
+       
+        // With the category ID we can now delete it.
+        $this->client->request('GET', '/category/delete/'.$categoryID);
+    }
+
+    /**
+     * @Then la categorie :cate n'est plus affichée dans l'application
+     */
+    public function laCategorieNestPlusAfficheeDansLapplication($cate)
+    {
+         $crawler = $this->client->request('GET', '/category');
+	
+        // By default there is already one card for the category creation 
+        // which has the same class
+        $category_count = $this->getItemCardCount($crawler, $cate);  
+        
+	$this->assertEquals(0, $category_count, "Category '". $cate ."' count is incorrect");
+    }   
 
     
     /**
@@ -111,6 +148,7 @@ class FeatureContext extends WebTestCase implements Context
         $form->setValues(array('appbundle_product[name]' => $product));
         // submit the form
         $this->client->submit($form);
+        
     }
 
     /**
@@ -120,18 +158,96 @@ class FeatureContext extends WebTestCase implements Context
     {    
 	$crawler = $this->client->request('GET', '/product');
         
-        $product_count = $crawler->filter('.card-image-label')->reduce(
-                function ($node, $i) use( $product) {                      
-                    if (strcmp(trim($node->text()), $product) == 0) {
+        $product_count = $this->getItemCardCount($crawler, $product);     
+        
+	$this->assertEquals(1, $product_count, "The product '". $product."' count is incorrect.");    
+    }
+
+    /**
+     * @Given il existe le produit :product dans l'application
+     */
+    public function ilExisteLeProduitDansLapplication($product)
+    {
+        // First I create a category.
+        $crawler = $this->client->request('GET', '/category');
+      
+        $form = $crawler->selectButton('Valider')->form();
+        // Set the category values
+        $form->setValues(array('appbundle_category[name]' => "testCategory"));
+        // submit the form
+        $this->client->submit($form);     
+        
+        // Then I create a product.
+        $crawler2 = $this->client->request('GET', '/product');
+      
+        $form2 = $crawler2->selectButton('Valider')->form();
+        // Set the product values
+        $form2->setValues(array('appbundle_product[name]' => $product));
+        // submit the form
+        $this->client->submit($form2);   
+    }
+
+    /**
+     * @When je supprime le produit :product dans l'application
+     */
+    public function jeSupprimeLeProduitDansLapplication($product)
+    {
+        $crawler = $this->client->request('GET', '/category');
+        // Filter to find the correct onclick attribute for the product.
+        $filter = "[onclick*=\"deleteProduct('". $product ."',\"]";
+       
+        $productID = $this->getItemCardId($crawler, $filter);
+       
+        // With the product ID we can now delete it.
+        $this->client->request('GET', '/product/delete/'.$productID);
+    }
+
+    /**
+     * @Then le produit :product n'est plus affichée dans l'application
+     */
+    public function leProduitNestPlusAfficheeDansLapplication($product)
+    {
+        $crawler = $this->client->request('GET', '/product');
+        
+        $product_count = $this->getItemCardCount($crawler, $product);     
+        
+	$this->assertEquals(0, $product_count, "The product '". $product."' count is incorrect.");    
+    }
+
+    /**
+     * 
+     * @param type $crawler The crawler with the HTML DOM from the page we want loaded.
+     * @param type $label Count the occurrences for the item with the given label
+     * @return type The number of item with the given name that appears on the page 
+     */
+    public function getItemCardCount($crawler, $label)
+    {
+        return $crawler->filter('.card-image-label')->reduce(
+                function ($node, $i) use($label) {
+                    // If the item text match the given text, keep it in the node list.
+                    if (strcmp(trim($node->text()), $label) == 0) {
                         return true;
                     } else {
                         return false;
                     }
                 }
+        // return the number of item in the list.
         )->count();
-        
-	$this->assertEquals(1, $product_count, "The product '". $product."' count is incorrect.");    
+    }        
+    
+    public function getItemCardId($crawler, $filter)
+    {
+        // Filter to find the correct onclick attribute for the product.
+        //$filter = "[onclick*=\"deleteProduct('". $product ."',\"]";
+        echo "Filter = ".$filter;
+        // On the js function, we can find the item ID.
+        $node_attribute = $crawler->filter($filter)->attr("onclick");
+        echo "Node attribute = ".$node_attribute;
+        // The product ID is located in the 4th index.
+        $itemID = explode("'", $node_attribute)[3];
+        return $itemID;
     }
+      
     
     /** @AfterScenario */
     public function after(AfterScenarioScope $scope)
